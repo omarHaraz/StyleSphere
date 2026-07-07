76dd2e1c056f95920039ae946898ad3e05133d58
omarHaraz <omarharaz553@gmail.com>
2026-07-07 10:47:23 +0300

    add AuthController for login

---
+++ backend/src/main/java/com/StyleSphere/backend/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.StyleSphere.backend.controller;
+
+import com.StyleSphere.backend.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController
+{
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        String jwt = tokenProvider.generateToken(authentication.getName());
+
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+
+
+}
