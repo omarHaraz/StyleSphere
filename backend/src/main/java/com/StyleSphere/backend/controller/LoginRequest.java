5edc526bfa1c041ea94fa563d5b4abd6113b8f14
omarHaraz <omarharaz553@gmail.com>
2026-07-08 13:12:56 +0300

    the login page final patch

--- backend/src/main/java/com/StyleSphere/backend/controller/LoginRequest.java
+++ backend/src/main/java/com/StyleSphere/backend/controller/LoginRequest.java
@@ -5,6 +5,8 @@ public class LoginRequest
     private String email;
     private String password;
 
+    public LoginRequest() {}
+
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
     public String getPassword() { return password; }
